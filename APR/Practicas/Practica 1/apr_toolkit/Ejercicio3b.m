22cf4a25c1ddbf8e3a43f0a5c97882e133277db6
Josep - Mac <jodoldar@users.noreply.github.com>
2017-10-12 16:34:58 +0200

    Ejercicio 3 acabado
    Se ha terminado el ejercicio 3 de la practica, que ya muestra por pantalla los datos correctamente

--- APR/Practicas/Practica 1/apr_toolkit/Ejercicio3b.m
+++ APR/Practicas/Practica 1/apr_toolkit/Ejercicio3b.m
@@ -1,13 +1,15 @@
+clear;
 importdata("./data/mini/tr.dat",' ',4);
 tr = ans.data;
 importdata("./data/mini/trlabels.dat",' ',4);
 trlabels = ans.data;
+
 C = 1000;
 axis([0 7 0 7]);
-
+grid on
 hold on
-scatter(tr(trlabels==1,1),tr(trlabels==1,2),'ob','filled');
-scatter(tr(trlabels==2,1),tr(trlabels==2,2),'sg','filled');
+scatter(tr(trlabels==1,1),tr(trlabels==1,2),50,'sr','filled');
+scatter(tr(trlabels==2,1),tr(trlabels==2,2),50,'ob','filled');
 
 input = sprintf('-t 0 -c %d',C);
 res=svmtrain(trlabels,tr,input);
@@ -18,11 +20,13 @@ fprintf("Vectores soporte: \n");
 disp(full(res.SVs));
 
 alfa = res.sv_coef;
-%clase = trlabels(res.sv_indices,1);
+clase = trlabels(res.sv_indices,1);
+clase(clase==2)=-1;
 equis = tr(res.sv_indices,:);
 mult = alfa.*equis;
 sigma = sum(mult);
-sigma0  = clase(1)-sigma*equis(1,:)';
+%sigma0  = clase(1)-sigma*equis(1,:)';
+sigma0 = -1*res.rho;
 
 fprintf("Vector de pesos: ");
 disp(sigma);
@@ -56,7 +60,9 @@ alfa2 = alfa(alfaPrima>0,:);
 val2 = (sigma*equis2'+sigma0);
 val3 = abs(round(val2,2));
 equisErr = equis2(val3~=1,:);
-scatter(equisErr(:,1),equisErr(:,2),200,'xr');
+scatter(equisErr(:,1),equisErr(:,2),400,'xr','LineWidth',1.5);
+equisErr = equis2(val3==1,:);
+scatter(equisErr(:,1),equisErr(:,2),200,'+k');
 
 nAlfa = abs(alfa);
 equis3 = equis(alfaPrima<=0,:);
@@ -65,9 +71,21 @@ val0 = (sigma*equis3'+sigma0)-1;
 val1 = ((-1*val0).*abs(alfa3'))./abs(alfa3)';
 val2 = abs(round(val1,2));
 equisErr = equis3(val2>1,:);
-scatter(equisErr(:,1),equisErr(:,2),200,'xr');
-hold off
+scatter(equisErr(:,1),equisErr(:,2),400,'xr','LineWidth',1.5);
 
-%val2 = (sigma*equis2'+sigma0);
-%val1 = val0-1;
-%cedillas = ((-1*val1).*nAlfa')./nAlfa';
+textOut = tr(setdiff((1:size(tr,1)),res.sv_indices),:);
+text(textOut(:,1)-0.05,textOut(:,2)+0.1,'0.00\newline    0.00');
+
+
+
+equisAux = full(res.SVs);
+val0 = clase'.*(sigma*equisAux'+sigma0);
+val1 = val0-1;
+cedillas = abs(round(((-1*val1).*alfa')./alfa',2));
+
+textOut = equisAux;
+for i = 1:size(textOut,1)
+    text1 = sprintf('%.2f\n    %.2f',nAlfa(i),cedillas(i));
+    text(textOut(i,1)-0.05,textOut(i,2)+0.1,text1);
+end
+hold off
